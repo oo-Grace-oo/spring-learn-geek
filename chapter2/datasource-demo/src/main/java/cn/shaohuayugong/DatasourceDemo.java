da656d68402f55574d96bf69713bd984e1b141d0
Grace <lxy1123598050@gmail.com>
2023-12-07 00:06:56 +0800

    单数据源学习

---
+++ chapter2/datasource-demo/src/main/java/cn/shaohuayugong/DatasourceDemo.java
@@ -0,0 +1,43 @@
+package cn.shaohuayugong;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author ShaoHuaYuGong
+ * @date 2023/12/06
+ */
+@SpringBootApplication
+@Slf4j
+public class DatasourceDemo implements CommandLineRunner {
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    public void run(String... args) throws Exception {
+        showConnection();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatasourceDemo.class, args);
+    }
+
+    private void showConnection() throws SQLException {
+        log.warn(dataSource.toString());
+        Connection conn = dataSource.getConnection();
+        log.warn(conn.toString());
+        conn.close();
+    }
+}
---
+++ chapter2/pure-spring-datesource-demo/src/main/java/cn/shaohuayugong/PureSpringDataSourceDemo.java
@@ -0,0 +1,71 @@
+package cn.shaohuayugong;
+
+import org.apache.commons.dbcp2.BasicDataSourceFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author ShaoHuaYuGong
+ * @date 2023/12/06
+ */
+@SpringBootApplication
+@Configuration
+@EnableTransactionManagement
+public class PureSpringDataSourceDemo {
+
+
+    @Autowired
+    private DataSource dataSource;
+
+    public static void main(String[] args) {
+        SpringApplication.run(PureSpringDataSourceDemo.class, args);
+    }
+
+    @Bean(destroyMethod = "close")
+    public DataSource dataSource() throws Exception {
+        Properties properties = new Properties();
+        properties.setProperty("driverClassName", "org.h2.Driver");
+        properties.setProperty("url", "jdbc:h2:mem:testdb");
+        properties.setProperty("username", "sa");
+        return BasicDataSourceFactory.createDataSource(properties);
+
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() throws Exception {
+        return new DataSourceTransactionManager(dataSource());
+    }
+
+    private static void showBeans(ApplicationContext applicationContext) {
+        System.out.println(Arrays.toString(applicationContext.getBeanDefinitionNames()));
+    }
+
+    private static void dataSourceDemo(ApplicationContext applicationContext) throws SQLException {
+        PureSpringDataSourceDemo demo = applicationContext.getBean("dataSourceDemo", PureSpringDataSourceDemo.class);
+        demo.showDataSource();
+    }
+
+    public void showDataSource() throws SQLException {
+        System.out.println(dataSource.toString());
+        Connection conn = dataSource.getConnection();
+        System.out.println(conn.toString());
+        conn.close();
+    }
+}
