5b7a26017ec82470e0b4ee3511b1c81214054285
Grace <lxy1123598050@gmail.com>
2023-12-11 23:27:28 +0800

    添加MySQL的数据源配置

--- chapter2/datasource-demo/src/main/java/cn/shaohuayugong/DatasourceDemo.java
+++ chapter2/datasource-demo/src/main/java/cn/shaohuayugong/DatasourceDemo.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -25,9 +26,12 @@ public class DatasourceDemo implements CommandLineRunner {
     @Autowired
     private DataSource dataSource;
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
     @Override
     public void run(String... args) throws Exception {
         showConnection();
+        showData();
     }
 
     public static void main(String[] args) {
@@ -39,5 +43,9 @@ public class DatasourceDemo implements CommandLineRunner {
         Connection conn = dataSource.getConnection();
         log.warn(conn.toString());
         conn.close();
+    }
+    private void showData() {
+        jdbcTemplate.queryForList("SELECT * FROM FOO")
+                .forEach(row -> log.warn(row.toString()));
     }
 }
