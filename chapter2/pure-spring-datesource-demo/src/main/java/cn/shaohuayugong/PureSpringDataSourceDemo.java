437aec4c79d6d599dfd6bed6e992861504b21e92
Grace <lxy1123598050@gmail.com>
2023-12-11 22:12:13 +0800

    单数据源学习

--- chapter2/pure-spring-datesource-demo/src/main/java/cn/shaohuayugong/PureSpringDataSourceDemo.java
+++ chapter2/pure-spring-datesource-demo/src/main/java/cn/shaohuayugong/PureSpringDataSourceDemo.java
@@ -1,21 +1,15 @@
 package cn.shaohuayugong;
 
-import org.apache.commons.dbcp2.BasicDataSourceFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Arrays;
-import java.util.Properties;
 
 /**
  * <p>
@@ -25,40 +19,40 @@ import java.util.Properties;
  * @author ShaoHuaYuGong
  * @date 2023/12/06
  */
-@SpringBootApplication
 @Configuration
 @EnableTransactionManagement
 public class PureSpringDataSourceDemo {
 
-
     @Autowired
     private DataSource dataSource;
 
-    public static void main(String[] args) {
-        SpringApplication.run(PureSpringDataSourceDemo.class, args);
+    public static void main(String[] args) throws SQLException {
+        ApplicationContext applicationContext =
+                new ClassPathXmlApplicationContext("applicationContext*.xml");
+        showBeans(applicationContext);
+        dataSourceDemo(applicationContext);
     }
 
-    @Bean(destroyMethod = "close")
-    public DataSource dataSource() throws Exception {
-        Properties properties = new Properties();
-        properties.setProperty("driverClassName", "org.h2.Driver");
-        properties.setProperty("url", "jdbc:h2:mem:testdb");
-        properties.setProperty("username", "sa");
-        return BasicDataSourceFactory.createDataSource(properties);
+    // @Bean(destroyMethod = "close")
+    // public DataSource dataSource() throws Exception {
+    //     Properties properties = new Properties();
+    //     properties.setProperty("driverClassName", "org.h2.Driver");
+    //     properties.setProperty("url", "jdbc:h2:mem:testdb");
+    //     properties.setProperty("username", "sa");
+    //     return BasicDataSourceFactory.createDataSource(properties);
+    // }
 
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager() throws Exception {
-        return new DataSourceTransactionManager(dataSource());
-    }
+    // @Bean
+    // public PlatformTransactionManager transactionManager() throws Exception {
+    //     return new DataSourceTransactionManager(dataSource());
+    // }
 
     private static void showBeans(ApplicationContext applicationContext) {
         System.out.println(Arrays.toString(applicationContext.getBeanDefinitionNames()));
     }
 
     private static void dataSourceDemo(ApplicationContext applicationContext) throws SQLException {
-        PureSpringDataSourceDemo demo = applicationContext.getBean("dataSourceDemo", PureSpringDataSourceDemo.class);
+        PureSpringDataSourceDemo demo = applicationContext.getBean("pureSpringDataSourceDemo", PureSpringDataSourceDemo.class);
         demo.showDataSource();
     }
 
